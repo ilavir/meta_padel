0d37f9b8818095ef562c32c70ed4479c2f8c8f02
ilavir <1428978+ilavir@users.noreply.github.com>
2025-06-12 12:54:16 +0300

    add error messages display in base.html

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -14,8 +14,6 @@ def login():
     if form.validate_on_submit():
         logger.info(f'Login form submitted. Username: {form.username.data}, Remember Me: {form.remember_me.data}')
         return redirect('/')
-    else:
-        logger.warning(f'Login form validation failed. Errors: {form.errors}')
 
     return render_template('auth/login.html', title='Вход', form=form)
 
@@ -27,7 +25,5 @@ def register():
     if form.validate_on_submit():
         logger.info(f'Registration form submitted. Username: {form.username.data}')
         return redirect(url_for('auth.login'))
-    else:
-        logger.warning(f'Login form validation failed. Errors: {form.errors}')
 
     return render_template('auth/register.html', title='Регистрация', form=form)
